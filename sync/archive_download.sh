b3170c15dac180996afbf2a126da325e2ffd6040
jamesmurphy0622 <james.murphy0622@gmail.com>
2022-05-25 10:53:12 -0700

    fix: rm redownload utilities

--- sync/archive_download.sh
+++ sync/archive_download.sh
@@ -39,28 +39,17 @@ fi
 run_download() {
   #  https://aria2.github.io/manual/en/html/aria2c.html
   # Use file allocation so if the disk fills up the process should exit.
-  curl -s $1 | aria2c -d $2 -c --auto-file-renaming=false --file-allocation=falloc --no-file-allocation-limit=1M -j 25 -i -
-}
-
-run_with_resync() {
-  # download primary manifest
-  MANIFEST=$(curl -s $1)
-  primary_manifest_hash=$(echo $MANIFEST | md5sum)
-  run_download $1 $2
-
-  MANIFEST=$(curl -s $1)
-  if [[ $primary_manifest_hash != $(echo -e $MANIFEST | md5sum) ]] ;
-  then
-    echo "Running download again as source has been updated since the initial download was started."
-    # TODO: Implement a diff to only download new files
-#    grep -Fxv $(curl -s $1) $($primary_manifest_hash)
-    run_download $1 $2
-  else
-    echo "No need to download again. Up to date."
-  fi
+  curl -s $1 | aria2c \
+    -d $2 -c --auto-file-renaming=false \
+    --file-allocation=falloc \
+    -l log-download.txt \
+    --no-file-allocation-limit=1M \
+    -j 25 \
+    -i -
 }
 
 echo "Starting primary download..."
-run_with_resync $PRIMARY_MANIFEST_LOCAvi TION $2
+run_download $PRIMARY_MANIFEST_LOCATION $2
 echo "Starting parachains download..."
-run_with_resync $PARACHAINS_MANIFEST_LOCATION $2/parachains/db
+run_download $PARACHAINS_MANIFEST_LOCATION $2/parachains/db
+
